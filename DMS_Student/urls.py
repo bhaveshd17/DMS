ba3e85932568500571ea9bb56d7d75be2d924173
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-06-12 23:10:45 +0530

    template added

--- DMS_Student/urls.py
+++ DMS_Student/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('student/', views.index, name='index')
+    path('student/', views.index, name='index'),
+    path('student/internship', views.internship, name='internship'),
+    path('student/preplacement', views.preplacement, name='preplacement'),
 ]
--- DMS_Student/views.py
+++ DMS_Student/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def index(request):
     content = {}
     return render(request, 'student/index.html', content)
+
+def internship(request):
+    content = {}
+    return render(request, 'student/internship.html', content)
+
+def preplacement(request):
+    content = {}
+    return render(request, 'student/preplacement.html', content)
