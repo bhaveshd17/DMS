5f8b2af29728c27b5b6fa87b25c89f73022deb94
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-06-13 14:09:57 +0530

    models created

--- DMS_Student/admin.py
+++ DMS_Student/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(student)
+admin.site.register(add_edu)
+admin.site.register(add_exp)
+admin.site.register(adminDma)
+admin.site.register(intership)
+admin.site.register(int_user)
+admin.site.register(job)
+admin.site.register(job_user)
+admin.site.register(login)
+admin.site.register(mock_test)
+
+
