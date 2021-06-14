dd1e374d12015e43b53a6b21834538a4de76f3ac
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-06-14 14:23:43 +0530

    Skill logic added

--- DMS_Student/views.py
+++ DMS_Student/views.py
@@ -1,62 +1,41 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from .models import *
 from django.contrib.auth import authenticate, login, logout
-
 from django.contrib.auth.decorators import login_required
-from .decorators import unauthenticated_user
 
+from .models import *
+from .decorators import unauthenticated_user
 from .form import SkillsForm
+from .utils import IntershipJobLogic
 
 @login_required(login_url='login')
 def index(request):
-    roll_no = request.user.username
-    student_obj = Student.objects.get(roll_no=roll_no)
-    skills = student_obj.skills
-    sk_list = skills.split(',')
-    sk_set = set()
-    for skill in sk_list:
-        job = Job.objects.filter(skills__icontains=skill)
-        if not job.count():
-            continue
-        sk_set.add(job[0].skills)
-    print(sk_set)
-
-    job_obj = Job.objects.filter(skills__icontains=skills)
-    print(job_obj)
-    job_list = []
-    for job in job_obj:
-        job_list.append(job)
-
-    int_obj = Intership.objects.all()
-    int_list = []
-    for intern in int_obj:
-        int_list.append(intern)
-    
-    content = {'job_list':job_list, 'int_list': int_list[:3]}
+    data = IntershipJobLogic(request)
+    content = {'related_job_list': data['related_job_list'][:3], 'related_int_list': data['related_int_list'][:3]}
     return render(request, 'student/index.html', content)
 
 
 @login_required(login_url='login')
 def internship(request):
-    int_obj = Intership.objects.all()
-    content = {'int_obj':int_obj}
+    data = IntershipJobLogic(request)
+    content = {'related_int_list': data['related_int_list']}
     return render(request, 'student/internship.html', content)
 
 
 @login_required(login_url='login')
 def preplacement(request):
-    mock_test= Mock_test.objects.all()
-    content = {'mock_test':mock_test}
+    data = IntershipJobLogic(request)
+    content = {'mock_test':data['mock_list']}
     return render(request, 'student/preplacement.html', content)
 
 
 @login_required(login_url='login')
 def job(request):
-    job_obj = Job.objects.all()
-    content = {'job_obj':job_obj}
+    data = IntershipJobLogic(request)
+    content = {'related_job_list': data['related_job_list']}
     return render(request, 'student/job.html', content)
 
+@login_required(login_url='login')
 def details(request,id,type):
     if type==1:
         job_obj = Job.objects.get(id=id)
@@ -90,6 +69,7 @@ def profile(request):
 
     return render(request,"student/profile.html",content)
 
+@login_required(login_url='login')
 def UpdateSkills(request):
     rollNo=request.user.username
     student=Student.objects.get(roll_no=rollNo)
@@ -97,9 +77,11 @@ def UpdateSkills(request):
         form=SkillsForm(request.POST,instance=student)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Successfully Updated')
             return redirect('/student/profile')
         else:
             messages.error(request, 'Invalid Update')
+            return redirect('/student/profile')
 
 
 # authentication
