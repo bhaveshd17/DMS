37e401bc5e35f69888790e5d29741f15cf7e77cc
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-06-30 18:43:59 +0530

    company app

--- DMS/urls.py
+++ DMS/urls.py
@@ -6,11 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('DMS_Student.urls')),
-<<<<<<< HEAD
     path('placement_cell/', include('DMS_Placement_Cell.urls')),
-=======
     path('company/',include('DMS_Company.urls')),
->>>>>>> 408326523106c77293ff5435a75682c0ab5c7692
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
