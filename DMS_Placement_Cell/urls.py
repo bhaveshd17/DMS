2b9f64e16becf9e7d3f3ab7c8648fc65bc809278
chinmay2902 <chinmaynaphade29@gmail.com>
2021-07-02 13:23:57 +0530

    merage

--- DMS_Placement_Cell/urls.py
+++ DMS_Placement_Cell/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("add_intership/",views.add_intership,name="add_intership"),
     path("add_job/",views.add_job,name="add_job"),
     path("form_intership",views.form_intership,name="form_intership"),
-    path("form_job",views.form_job,name="form_job"),
+    # path("form_job",views.form_job,name="form_job"),
     path("recruiting/",views.recruiting,name="recruiting"),
     path("recruited/",views.recruited,name="recruited"),
 ]
