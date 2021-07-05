a80ae795af5320b99fbcbde3fecb72109330b111
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-07-05 23:59:02 +0530

    diploma

--- DMS_Student/views.py
+++ DMS_Student/views.py
@@ -50,23 +50,7 @@ def internshipFilter(request):
 @login_required(login_url='login')
 def job(request):
     data = jobLogic(request)
-    try:
-        job_list = data['related_job_list']
-        student = Student.objects.get(roll_no=request.user.username)
-        cgpa = BE.objects.get(roll_no_4=student).be_cgpa
-        live_kt = BE.objects.get(roll_no_4=student).kt_BE
-        drop_2 = SE.objects.get(roll_no_2=student).drop_SE
-        drop_3 = TE.objects.get(roll_no_3=student).drop_TE
-        drop_4 = BE.objects.get(roll_no_4=student).drop_BE
-        drop = int(drop_2) + int(drop_3) + int(drop_4)
-        related_job_list = []
-        for job in job_list:
-            if job.cgpa <= cgpa and int(live_kt) <= int(job.live_kt) and int(drop) <= int(job.drop):
-                related_job_list.append(job)
-    except:
-        related_job_list = []
-
-    content = {'related_job_list': related_job_list,
+    content = {'related_job_list': data['related_job_list'],
                'skill_set':data['skill_set'],
                'cities':"Mumbai,Bangalore,Chennai".split(','),
                }
