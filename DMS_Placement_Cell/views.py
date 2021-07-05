d2c4d532cbfc18d1cb517030288dd932e0fa26f6
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-07-05 13:45:38 +0530

    admin

--- DMS_Placement_Cell/views.py
+++ DMS_Placement_Cell/views.py
@@ -9,8 +9,8 @@ from django.contrib.auth.models import User
 
 @allowed_users(allowed_roles=['Placement_Cell'])
 def index(request):
-    int_user = Int_user.objects.all().order_by("id")
-    job_user = Job_user.objects.all().order_by("id")
+    int_user = Int_user.objects.all().order_by("int_id__comp_name")
+    job_user = Job_user.objects.all().order_by("job_id__comp_name")
     content = {'int_user':int_user, 'job_user':job_user}
     return render(request, 'placement/index.html', content)
 
