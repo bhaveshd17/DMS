fa51eb5ac7b69db7c9d7bb53d002e211a0b51ec0
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-07-22 21:10:31 +0530

    v1

--- DMS/settings.py
+++ DMS/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-&+m0zlce=4mk9yw-@k#m+^sh)3xm=ygu0mvvwgag*)9u=#50h#
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vplacement.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
--- DMS/urls.py
+++ DMS/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('placement_cell/', include('DMS_Placement_Cell.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
