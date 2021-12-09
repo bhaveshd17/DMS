7b7963614268279632ab1a6c238c27d747908198
chinmay2902 <chinmaynaphade29@gmail.com>
2021-12-09 18:27:11 +0530

    pull

--- DMS/settings.py
+++ DMS/settings.py
@@ -130,5 +130,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # email sending function is close because of real data
 # EMAIL_HOST_PASSWORD = 'ucvsrtsbguawprna'
-EMAIL_HOST_PASSWORD = ''
+# EMAIL_HOST_PASSWORD = ''
 
